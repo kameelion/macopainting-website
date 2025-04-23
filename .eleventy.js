c9953babb0fc6b80b202ad89ff333d505621c5f9
kameelion <grazeprotocol@gmail.com>
2025-04-23 00:42:54 +0000

    Enable passthrough for public folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/scripts"); // Added scripts directory
+  eleventyConfig.addPassthroughCopy("public"); // 🚀 Add this line to copy the public folder
 
   return {
     dir: {
