d5246d615e00713eae3561e603767a02029a7445
kameelion <grazeprotocol@gmail.com>
2025-04-23 03:03:42 +0000

    Final sitemap.xml complete and verified

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
+const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function (eleventyConfig) {
+  // Pass through directories
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/scripts"); // Scripts directory
+
+  // Sitemap plugin setup
+  eleventyConfig.addPlugin(pluginSitemap, {
+    sitemap: {
+      hostname: "https://www.macopainting.com",
+    },
+  });
 
   return {
     dir: {
@@ -14,5 +24,18 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
+    templateFormats: ["njk", "html", "xml"], // Add XML support here
+    // Force Eleventy to include all Nunjucks pages (index.html, sitemap.xml)
+    eleventyComputed: {
+      permalink: (data) => {
+        if (data.page.filePathStem && data.page.inputPath.endsWith(".njk")) {
+          if (data.page.filePathStem.endsWith("sitemap.xml")) {
+            return "/sitemap.xml";
+          }
+          return data.page.filePathStem + "/index.html";
+        }
+        return false;
+      },
+    },
   };
 };
